fd9be0a542c5ff666897a1fb7576cf36d28d161d
Vicky8174 <91714812+Vicky8174@users.noreply.github.com>
2024-02-24 15:04:28 +0530

    Update Header.jsx

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -12,27 +12,28 @@ const Header = () => {
       <header>
         <nav className='flexSB'>
           <ul className={click ? "mobile-nav" : "flexSB "} onClick={() => setClick(false)}>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/courses'>All Courses</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/team'>Team</Link>
-            </li>
-            <li>
-              <Link to='/pricing'>Pricing</Link>
-            </li>
-            <li>
-              <Link to='/journal'>Journal</Link>
-            </li>
-            <li>
-              <Link to='/contact'>Contact</Link>
-            </li>
+          <li>
+  <a href='/'>Home</a>
+</li>
+<li>
+  <a href='/courses'>All Courses</a>
+</li>
+<li>
+  <a href='/about'>About</a>
+</li>
+<li>
+  <a href='/team'>Team</a>
+</li>
+<li>
+  <a href='/pricing'>Pricing</a>
+</li>
+<li>
+  <a href='/journal'>Journal</a>
+</li>
+<li>
+  <a href='/contact'>Contact</a>
+</li>
+
           </ul>
           <div className='start'>
             <div className='button'>GET CERTIFICATE</div>
